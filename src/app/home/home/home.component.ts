f9b47d5637d3afa838ab89b993324397d60d7828
alanfg120 <alanfg120@gmail.com>
2020-10-09 15:22:44 -0500

    rutas change

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -9,12 +9,15 @@ export class HomeComponent implements OnInit {
   programacion = [];
   participantes = [];
   emprendedores = [];
+  mapa1 : string;
+  mapa2 : string;
 
   constructor(public dataService: HomeService) {}
 
   ngOnInit(): void {
     this.dataService.getdatos().subscribe((data) => {
-
+     this.mapa1 = data.mapa1;
+     this.mapa2 = data.mapa2;
      this.programacion = data.programacion;
      this.participantes = data.participantes;
      this.emprendedores = data.emprendimiento;
